4ba1e2ae6a35a36a48310cb447b66c994dbfb4a1
GaRyung <shubom0115@naver.com>
2024-11-29 17:53:57 +0900

    Auto stash before merge of "main" and "origin/main"

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -1,23 +1,25 @@
 import 'dart:io';
 
+import 'package:bookapplication/user/kakaologinapi.dart';
+import 'package:bookapplication/user/usercontroller.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
-// class MyLogin extends StatelessWidget {
-//   const MyLogin({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ChangeNotifierProvider(
-//       create: (_) => UserController(
-//         kakaoLoginApi: KakaoLoginApi(),
-//       ),
-//       child: const MaterialApp(
-//         home: LoginScreen(),
-//       ),
-//     );
-//   }
-// }
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => UserController(
+        kakaoLoginApi: KakaoLoginApi(),
+      ),
+      child: const MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
+  }
+}
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) :super(key: key);
@@ -64,9 +66,15 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget kakaoLogin () {
-    return Center(
-
-    );
-  }
+  Widget kakaoLogin () => Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: GestureDetector(
+      // 카카오 로그인을 위한 메소드
+        onTap: () {
+          context.read<UserController>().kakaoLogin;
+          KakaoLoginApi().signWithKakao(context); // context 전달
+        },
+        child: Image.asset("assets/image/kakao_login_medium_narrow.png")),
+  );
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:bookapplication/home/home_screen.dart';
 import 'package:bookapplication/library/search_sub.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 import 'library/search_screen.dart';
 import 'package:bookapplication/login/login_screen.dart';
@@ -13,10 +14,21 @@ import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: "896d75d960f3df2e239b38c2eb220039",
+    javaScriptAppKey: "6cd5b407e4dc629bed984fb6399f17c3",
+  );
   // await initializeDateFormatting();
   runApp(
     MaterialApp(
-      home: LoginScreen(),
+      debugShowCheckedModeBanner: false, // 디버그 배너 비활성화
+      initialRoute: '/', // 초기 라우트 (로그인 화면)
+      routes: {
+        '/': (context) => const LoginScreen(), // 로그인 페이지
+        '/main': (context) => const HomeScreen(), // 메인 페이지
+      },
       // home: HomeScreen(),
     )
   );
