6684a436f77d4bcaff8ba0edd22e7bd191e86fe6
seon <sunkyoung.dev@gmail.com>
2024-11-29 17:18:36 +0900

    검색 결과 조건에 맞게 도서관 스케줄 카드 나열

--- lib/library/search_screen.dart
+++ lib/library/search_screen.dart
@@ -3,11 +3,15 @@
 
 
 
+import 'dart:convert';
+
 import 'package:bookapplication/home/schedule_card.dart';
 import 'package:bookapplication/library/search_result_banner.dart';
+import 'package:bookapplication/model/library_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:bookapplication/model/region.dart' ;
-
+import 'package:http/http.dart' as http;
+import 'package:bookapplication/const/config.dart';
 
 class SearchScreen extends StatefulWidget{
   const SearchScreen({Key? key}) :super(key: key);
@@ -24,10 +28,47 @@ class _SearchcreenState extends State<SearchScreen>{
   String siRegion="전체";
   String? doCode;
   String? siCode;
+  dynamic resultNum=0;
+  Map<String,dynamic>? rejson;
+  Map<String,dynamic>? libsInfo;
+  List<dynamic>?libsInfoList;
+  //resultNum=0;
   Map<String,String>? filterSiRegion;
   @override
   void initState() {
     super.initState();
+  }
+
+  String result='';
+  onPressGet(String doCode,String siCode) async{
+    String httpUrl;
+    if(siCode== null && doCode==null){
+     httpUrl='https://data4library.kr/api/libSrch?authKey=$libraryAUthKey&pageNo=1&pageSize=3&format=json';
+    }else{
+      httpUrl='https://data4library.kr/api/libSrch?authKey=$libraryAUthKey&region=$doCode&dtl_region=$siCode&pageNo=1&pageSize=10&format=json';
+    }
+    http.Response response= await http.get(
+
+        Uri.parse(httpUrl)
+
+    );
+
+    if(response.statusCode==200){
+      setState(() {
+       rejson=jsonDecode(response.body);
+       resultNum=rejson!['response']['resultNum'];
+       rejson=rejson?['response'];
+       libsInfo=Map.fromEntries(rejson!.entries.where((entry)=>entry.key=="libs"));
+       libsInfoList=libsInfo!["libs"];
+       print(libsInfoList?[0]);
+      });
+
+
+    }else{
+      setState(() {
+        result='error!';
+      });
+    }
   }
 
 
@@ -98,6 +139,7 @@ class _SearchcreenState extends State<SearchScreen>{
                                                     setState(() {
                                                       print('${key.substring(0,2)}');
                                                       doRegion=value;
+                                                      doCode=key;
                                                       filterSiRegion=Map.fromEntries(
                                                         region.dtlRegionCodes.entries.where((entry)=>entry.key.startsWith("${key.substring(0,2)}"))
                                                       );
@@ -139,6 +181,7 @@ class _SearchcreenState extends State<SearchScreen>{
                                                   child: TextButton(onPressed: (){
                                                     setState(() {
                                                       siRegion=value!;
+                                                      siCode=key;
                                                     });
                                                   }, child: Text('$value')),
 
@@ -158,16 +201,35 @@ class _SearchcreenState extends State<SearchScreen>{
                             ],
                           ),
                         ),
-                        IconButton(onPressed: (){}, icon:Icon(Icons.search))
+                        //검색 아이콘
+                        IconButton(onPressed: (){
+                          onPressGet(doCode!, siCode!);
+
+                        }, icon:Icon(Icons.search))
                       ],
 
                     ),
                   ],
                 ),
                 SizedBox(height: 15.0,),
-                SearchResultBanner(count: 10),
+                SearchResultBanner(count: resultNum),
                 SizedBox(height: 10.0,),
-                ScheduleCard(libName: '청양', address: '청양', tel: '청양', homepage: '청양', closed: '청양', operationTime: '청양'),
+                Expanded(
+                    child:ListView.builder(
+                        itemCount:libsInfoList?.length ,
+                        itemBuilder: (context,index){
+                          Map<String,dynamic> libInfo=libsInfoList?[index];
+                          return
+                            ScheduleCard(libName: '${libInfo['lib']['libName']}', address: '${libInfo['lib']['address']}', tel: '${libInfo['lib']['tel']}', homepage: '${libInfo['lib']['homepage']}', closed: '${libInfo['lib']['closed']}', operationTime: '${libInfo['lib']['operationTime']}');
+
+                        }
+                    ),
+
+
+                ),
+
+                //리스트로 빌더로 값을 뿌려야함.
+
 
 
               ]),
