18b9e0ecf2adf9d46fd6ddc6a541930da5145c44
seon <sunkyoung.dev@gmail.com>
2024-11-29 15:51:19 +0900

    메인

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:bookapplication/home/home_screen.dart';
+
+import 'package:bookapplication/library/search_sub.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+import 'library/search_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting();
+  // await initializeDateFormatting();
   runApp(
     MaterialApp(
-      home: HomeScreen(),
+      home: SearchScreen(),
     )
   );
 }
