5fc7d9e8ab7a8decf27ef6859b1b6e8ecd0245a2
seon <sunkyoung.dev@gmail.com>
2024-12-01 16:28:01 +0900

    카카오 유저 컨트롤러에서 정보 파이어베이스에 저장

--- lib/user/usercontroller.dart
+++ lib/user/usercontroller.dart
@@ -1,24 +1,50 @@
 import 'package:bookapplication/user/kakaologinapi.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
 class UserController with ChangeNotifier {
   User? _user;
   KakaoLoginApi kakaoLoginApi;
   User? get user => _user;
+  FirebaseFirestore db=FirebaseFirestore.instance;
+
+
+
+
 
   UserController({required this.kakaoLoginApi});
 
-
   // 카카오 로그인
   void kakaoLogin(BuildContext context) async {
-    kakaoLoginApi.signWithKakao(context).then((user) {
+
+    CollectionReference userRef=db.collection("users");
+
+
+    kakaoLoginApi.signWithKakao(context).then((user) async {
       // 반환된 값이 NULL이 아니라면
       // 정보 전달
       if (user != null) {
         _user = user;
+        print(_user);
+        DocumentReference documentRef= await userRef.add(userToFirebase(_user!));
         notifyListeners();
       }
     });
     }
+
+  Map<String,dynamic> userToFirebase(User user) {
+    Map<String,dynamic> fireBase={
+      'id':user.id,
+      'nickname':user.properties?['nickname'],
+      'thumbnail_image':user.properties?['thumbnail_image'],
+      'name':user.kakaoAccount?.name,
+      'email':user.kakaoAccount?.email,
+      'phone_number':user.kakaoAccount?.phoneNumber
+
+
+    };
+    return fireBase;
+  }
 }
