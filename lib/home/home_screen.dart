173f1b38dcb40947005f7a7bdb693b66cecc45b3
seon <sunkyoung.dev@gmail.com>
2024-12-01 16:37:25 +0900

    애러수정

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -7,7 +7,6 @@ import 'package:bookapplication/shard/menubottom.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-import '../bottom_menu.dart';
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({Key? key}) :super(key: key);
